25cfb5f18ebe8d250884f293767e0e4091f8632a
luccagian <gianlucca_campanha@outlook.com>
2026-03-20 19:18:02 -0300

    add txt / loading

---
+++ README.txt
@@ -0,0 +1,5 @@
+Gian Lucca Campanha Ribeiro (10438361)
+Gabriel Ferreira (10442043)
+Matheus Regatieri Olivieri (10409590)
+
+link para vídeo no Youtube: 
--- main.c
+++ main.c
@@ -1,12 +1,20 @@
+// Gian Lucca Campanha Ribeiro (10438361)
+// Gabriel Ferreira (10442043)
+// Matheus Regatieri Olivieri (10409590)
+
+// mostrar que programa está processando as etapas (carregando etapas...)
+
+
 #include <stdio.h>
 
 #define N 7
 
 int tabuleiro [N][N] = 
-{#,#,#,#,#,#,#},
-{#,#,#,#,#,#,#},
-{#,#,#,#,#,#,#},
-{#,#,#,#,#,#,#},
-{#,#,#,#,#,#,#},
-{#,#,#,#,#,#,#},
-{#,#,#,#,#,#,#}
+{#,#,1,1,1,#,#},
+{#,#,1,1,1,#,#},
+{1,1,1,1,1,1,1},
+{1,1,1,0,1,1,1},
+{1,1,1,1,1,1,1},
+{#,#,1,1,1,#,#},
+{#,#,1,1,1,#,#}
+
